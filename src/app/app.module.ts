f2c4b092d7b90ad8b013fbd81b2de8bf9c926f94
jfranco1701 <jfranco@unomaha.edu>
2018-11-01 17:53:28 -0500

    Updated primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    FavoriteComponent,
+    HomeComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule
