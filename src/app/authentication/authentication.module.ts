a8eb29001ac03832db447ceaf7cfcacad11bc092
jfranco1701 <jfranco@unomaha.edu>
2018-11-05 20:09:28 -0600

    Login component update

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -4,19 +4,20 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RouterModule } from '@angular/router';
 import { AuthenticationRoutes } from './authentication.routing';
+import { FormsModule } from '@angular/forms';
 
-
-import {CardModule} from 'primeng/card';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   imports: [
     CommonModule,
     CardModule,
+    InputTextModule,
     RouterModule.forChild(AuthenticationRoutes),
+    FormsModule,
   ],
-  declarations: [
-    LoginComponent,
-    RegisterComponent,
-  ]
+  declarations: [LoginComponent, RegisterComponent],
 })
-export class AuthenticationModule { }
+export class AuthenticationModule {}
--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  password: string;
+  emailAddress: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.password = '';
+    this.emailAddress = '';
   }
 
 }
